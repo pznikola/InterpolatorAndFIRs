a440a509c4f59712e960094530d00209d96a612d
Jakov_Rancic <md160274d@gmail.com>
2022-07-15 19:07:50 +0200

    DS warnings

--- DeltaSigma/DeltaSigma_greske.v
+++ DeltaSigma/DeltaSigma_greske.v
@@ -14,11 +14,10 @@ module DeltaSigma
 	input reset;
 
 reg signed [3:0]data_out;
-reg feedback_flag;
+//reg feedback_flag;
 reg signed [13:0]acc1;		//prekoracenje moze biti problem
 reg signed [13:0]acc2;
 reg signed [13:0] feedback;
-reg treci, drugi, prvi;
      
 always@(posedge clk or posedge reset) begin
     if(reset == 1) begin
@@ -31,52 +30,21 @@ always@(posedge clk or posedge reset) begin
 
     	acc2 <= acc1 + feedback + acc2;
         acc1 <= data_in + feedback + acc1;
+        data_out[3:0] = acc2[13:10];
         
         if(acc2 > 0) begin
-        	feedback_flag = 1;
-        	data_out[3] = 0; //pozitivno
-        
-        	if(acc2[12] == 1) begin
-        		data_out[2] = 1;
-        	end else begin
-        		data_out[2] = 0;
-        	end
-        	if(acc2[8] == 1) begin
-        		data_out[1] = 1;
-        	end else begin
-        		data_out[1] = 0;
-        	end
-        	if(acc2[4] == 1) begin
-        		data_out[0] = 1;
-        	end else begin
-        		data_out[0] = 0; //acc[0] je greska pri racunu 1 bit znak, 3*4=12, 1 bit greska, suma 14
-        	end
-        
-        end else begin
-        	feedback_flag = 0;
-        	data_out[3] = 1; //negativno
-        
-        	if(acc2[12] == 1) begin
-        		data_out[2] = 1;
-        	end else begin
-        		data_out[2] = 0;
-        	end
-        	if(acc2[8] == 1) begin
-        		data_out[1] = 1;
-        	end else begin
-        		data_out[1] = 0;
-        	end
-        	if(acc2[4] == 1) begin
-        		data_out[0] = 1;
-        	end else begin
-        		data_out[0] = 0;
-        	end
-        end
-        if(feedback_flag == 1) begin
+        	//feedback_flag = 1;
         	feedback <= 14'b01111111111111;  //najpozitivniji
+                
         end else begin
+        	//feedback_flag = 0;
         	feedback <= 14'b11111111111111;  //najnegativniji
         end
+        //if(feedback_flag == 1) begin
+        //	feedback <= 14'b01111111111111;  //najpozitivniji
+       // end else begin
+        //	feedback <= 14'b11111111111111;  //najnegativniji
+       // end
         
      end
      
